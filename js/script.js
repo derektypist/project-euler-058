6e973f031cae26838e979d7c5831249b4dc32b72
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-11-16 12:06:56 +0000

    Added spiralPrimes function at script.js

--- js/script.js
+++ js/script.js
@@ -14,3 +14,44 @@ function getPercentInfo() {
     // Display Information in the Browser
     document.getElementById("percentinfo").innerHTML = txt;
 }
+
+/*
+    Function to return the side length of the square spiral for which the
+    percent of primes along both diagonals falls below a given percent
+    spiralPrimes(50) returns 11
+    spiralPrimes(15) returns 981
+    spiralPrimes(10) returns 26241
+*/
+function spiralPrimes(percent) {
+    function isPrime(n) {
+        if (n<=3) {
+            return n>1;
+        } else if (n%2===0 || n%3===0) {
+            return false;
+        }
+
+        for (let i=5;i*i<=n;i+=6) {
+            if (n%i===0 || n % (i+2) === 0) return false;
+        }
+
+        return true;
+    }
+
+    let totalCount = 1;
+    let primesCount = 0;
+    let curNumber = 1;
+    let curSideLength = 1;
+    let ratio = 1;
+    const wantedRatio = percent/100;
+
+    while (ratio >= wantedRatio) {
+        curSideLength += 2;
+        for (let i=0;i<4;i++) {
+            curNumber += curSideLength - 1;
+            totalCount++;
+            if (i!==3 && isPrime(curNumber)) primesCount++;
+        }
+        ratio = primesCount/totalCount;
+    }
+    return curSideLength;
+}
